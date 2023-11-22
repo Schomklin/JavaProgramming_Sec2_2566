9d07a265919ec0ed2aa1f6726a62f66d7bed788c
Schomklin <kungchomklin@gmail.com>
2023-11-22 14:06:29 +0700

    JavaLab3(2)

---
+++ LabSheet03/src/Withdrawal.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.util.Random; //use library for Random class
+
+public class Withdrawal {
+	public static void main(String[] args) {
+		//using class Random for random money
+		Random rdmoney = new Random();
+		int balance = rdmoney.nextInt(9) * 100000;
+		//JOptionPane.showMessageDialog(null, "Your balance: "+ balance);
+		int withdraw = ;
+		
+		if(withdraw>2000) {
+			
+		}
+		else if(withdraw>balance) {
+			
+		}
+		else if() {
+			
+		}
+		else {
+			
+		}
+			
+		
+		
+
+	}
+
+}
